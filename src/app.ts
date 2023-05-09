c245b0f8e284827065366b69a44b6ad87a555d2b
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2023-05-08 22:51:53 -0300

    instalação do prisma e migrate

--- src/app.ts
+++ src/app.ts
@@ -1,3 +1,13 @@
 import fastify from 'fastify'
+import { PrismaClient } from '@prisma/client'
 
 export const app = fastify()
+
+const prisma = new PrismaClient()
+
+prisma.user.create({
+  data: {
+    name: 'Gabriel Alves',
+    email: 'gabrieldevalves@hotmail.com',
+  },
+})
