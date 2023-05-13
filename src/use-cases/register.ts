d790919e4d6e735932b0ee32bda45d97bc43144f
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2023-05-13 16:04:37 -0300

    :sparkles: Caso de Uso e Design Patterns - Interface do repositório

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,4 +1,4 @@
-import { prisma } from '@/lib/prisma'
+import { UsersRepository } from '@/repositories/users-repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
@@ -7,13 +7,10 @@ interface RegisterUseCaseRequest {
   password: string
 }
 export class RegisterUseCase {
-  constructor(private usersRepository: any) {}
+  constructor(private usersRepository: UsersRepository) {}
   async execute({ name, email, password }: RegisterUseCaseRequest) {
-    const userWithSameEmail = await prisma.user.findUnique({
-      where: {
-        email,
-      },
-    })
+    const userWithSameEmail = await this.usersRepository.findByEmail(email)
+
     if (userWithSameEmail) {
       throw new Error('E-mail already exists.')
     }
