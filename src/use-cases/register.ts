8bd307dfb52c1810bd4bd6bf72ff31d3fdeedfb1
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2023-05-13 15:55:01 -0300

    :sparkles: Caso de Uso e Design Patterns - Inversão de dependências

--- src/use-cases/register.ts
+++ src/use-cases/register.ts
@@ -1,5 +1,4 @@
 import { prisma } from '@/lib/prisma'
-import { PrismaUsersRepository } from '@/repositories/prisma-users-repository'
 import { hash } from 'bcryptjs'
 
 interface RegisterUseCaseRequest {
@@ -7,27 +6,24 @@ interface RegisterUseCaseRequest {
   email: string
   password: string
 }
+export class RegisterUseCase {
+  constructor(private usersRepository: any) {}
+  async execute({ name, email, password }: RegisterUseCaseRequest) {
+    const userWithSameEmail = await prisma.user.findUnique({
+      where: {
+        email,
+      },
+    })
+    if (userWithSameEmail) {
+      throw new Error('E-mail already exists.')
+    }
 
-export async function registerUseCase({
-  name,
-  email,
-  password,
-}: RegisterUseCaseRequest) {
-  const userWithSameEmail = await prisma.user.findUnique({
-    where: {
+    const password_hash = await hash(password, 6)
+
+    await this.usersRepository.create({
+      name,
       email,
-    },
-  })
-  if (userWithSameEmail) {
-    throw new Error('E-mail already exists.')
+      password_hash,
+    })
   }
-
-  const password_hash = await hash(password, 6)
-  const prismaUsersRepository = new PrismaUsersRepository()
-
-  await prismaUsersRepository.create({
-    name,
-    email,
-    password_hash,
-  })
 }
