25cd9a94129230fb3890f3b859cb326f006f0510
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2023-05-20 12:12:30 -0300

    :sparkles: feat: Implementando casos de uso - Caso de uso de check-in

--- src/use-cases/authenticate.ts
+++ src/use-cases/authenticate.ts
@@ -13,13 +13,13 @@ interface AuthenticateUseCaseResponse {
 }
 
 export class AuthenticateUseCase {
-  constructor(private UsersRepository: UsersRepository) {}
+  constructor(private usersRepository: UsersRepository) {}
 
   async execute({
     email,
     password,
   }: AuthenticateUseCaseRequest): Promise<AuthenticateUseCaseResponse> {
-    const user = await this.UsersRepository.findByEmail(email)
+    const user = await this.usersRepository.findByEmail(email)
 
     if (!user) {
       throw new InvalidCredentialsError()
