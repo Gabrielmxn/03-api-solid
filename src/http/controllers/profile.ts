748109efea4a79bf1fee9df6ea7b887a68623372
Gabriel Alves <gabriel.s.t.a.r.k@hotmail.com>
2023-06-03 12:18:10 -0300

    :sparkles: feat: Controllers & Testes E2E - Controller de perfil

--- src/http/controllers/profile.ts
+++ src/http/controllers/profile.ts
@@ -1,5 +1,18 @@
+import { makeGetUserProfileUseCase } from '@/use-cases/factories/make-get-user-profile-use.case'
 import { FastifyReply, FastifyRequest } from 'fastify'
 
 export async function profile(request: FastifyRequest, reply: FastifyReply) {
-  return reply.status(200).send()
+  console.log(request.user.sub)
+  const getUserProfile = makeGetUserProfileUseCase()
+
+  const { user } = await getUserProfile.execute({
+    userId: request.user.sub,
+  })
+
+  // REMOVE O PASSWORD
+  Reflect.deleteProperty(user, 'password_hash')
+
+  return reply.status(200).send({
+    user,
+  })
 }
